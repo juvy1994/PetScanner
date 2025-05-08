cd4d6434d4eeeb8f3873017c2d884996fed882fa
Carlos Flores <carlos_jhousef593@hotmail.com>
2025-05-07 23:08:21 -0500

    Se crean las interfaces y el proyecto de API Rest

---
+++ PS.API/Controllers/PetScanController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using PS.Infrastructure.Interfaces;
+using PS.Infrastructure.Services;
+
+namespace PS.API.Controllers
+{
+    [Route("api/pets")]
+    [ApiController]
+    public class PetScanController : ControllerBase
+    {
+        private readonly IImageAnalysisService _imageService;
+        private readonly IChatService _chatService;
+
+        public PetScanController(IImageAnalysisService imageService, IChatService chatService)
+        {
+            _imageService = imageService;
+            _chatService = chatService;
+        }
+    }
+}
---
+++ PS.Infrastructure/Interfaces/IImageAnalysisService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS.Infrastructure.Interfaces
+{
+    public interface IImageAnalysisService
+    {
+        Task<string> UploadToBlobAsync(IFormFile file);
+        Task<string> AnalysizeBreedFromImageUrlAsync(string imageUrl);
+    }
+}
